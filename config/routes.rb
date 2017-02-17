150d5c421788c79f27c1d8c30bf189c0c0a75008
tiago <tiagocesariob@hotmail.com>
2017-02-16 23:19:50 -0300

    coloquei agora

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
 root to:"static_pages#index"
 
 get 'sobre', to: 'static_pages#sobre'
 get 'contato', to: 'static_pages#contato'
+get 'entrar', to: 'sessions#new'
+post 'entrar', to: 'sessions#create'
+delete 'sair', to: 'sessions#destroy'
 
+resources :contacts
+resources :users, only: [:new, :create, :show]
 
 end
